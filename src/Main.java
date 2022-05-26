0b05c2ada6a39e78b4885f6c17e560d442138318
josesoto <josesoto@itop.es>
2022-05-26 10:08:47 +0100

    feat: added main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import utils.SuperList;
+
 public class Main {
     public static void main(String[] args) {
+
+        SuperList<Integer> list = new SuperList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.addElement(i);
+        }
+        System.out.println("ANTES DE BORRAR LOS ELEMENTOS");
+        list.showElements();
+        System.out.println("----lista reversa abajo----");
+        list.showReversedElements();
+
+        list.removeElement(3);
+
+        System.out.println("DESPUES DE BORRAR LOS ELEMENTOS");
+        list.showElements();
+        System.out.println("----lista reversa abajo----");
+        list.showReversedElements();
 
     }
 }
